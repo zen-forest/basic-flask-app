cd15ceef56e0fc0ccc209ea9d2f646c37bc60db7
metasignal <timcollinsdesign+metasignal@gmail.com>
2022-12-07 17:18:56 -0500

    very basic query parameters

--- views.py
+++ views.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 
 views = Blueprint(__name__,"views")
 
@@ -6,5 +6,9 @@ views = Blueprint(__name__,"views")
 def home():
   return render_template("index.html", name="Tim")
 
-
+@views.route("/profile")
+def profile():
+  args = request.args
+  name = args.get('name')
+  return render_template("index.html", name=name) 
 
